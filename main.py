4074e9a3910f222faabf2c826af7ae3142214b06
Edyth-K <kelvin.dcruz@gmail.com>
2025-05-04 18:01:56 -0500

    add CircleShape class in circleshape.py and Player class in player.py

--- main.py
+++ main.py
@@ -2,25 +2,38 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
+from player import Player
 from  constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers =(updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
+
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         pygame.Surface.fill(screen, (0,0,0))
 
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
+
+        # limit the framerate to 60fps
+        dt = clock.tick(60)/1000
 
 if  __name__ == "__main__":
     main()
